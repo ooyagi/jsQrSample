ce6803d9a56af56a221bc7176188395de1f298fc
ooyagi <t_oyagi@outlook.jp>
2023-11-08 15:21:09 +0900

    タイトル変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QrReader } from './QrReader';
 function App() {
   return (
     <>
-      <h1>QrTest</h1>
+      <h1>jsQR読み取りテスト</h1>
       <div className="card">
         <QrReader />
       </div>
